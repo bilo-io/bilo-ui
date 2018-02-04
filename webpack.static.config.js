c138948059708eb8f1803ff0f7d9153bb52e40cd
Bilo Lwabona <bilo.lwabona@gmail.com>
2018-02-04 12:55:15 +0200

    updated structure

--- webpack.static.config.js
+++ webpack.static.config.js
@@ -14,8 +14,8 @@ var config = {
         main: SRC + '/_demo/index.js'
     },
     output: {
-        path: path.resolve(__dirname, 'docs'),
-        filename: 'index.js',
+        path: path.resolve(__dirname, './'),
+        filename: 'docs.js',
         libraryTarget: 'umd'
     },
     // resolve: {
