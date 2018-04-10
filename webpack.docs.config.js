ca89b7b1e372dccc938c0b2c4a96de5af1448de4
Bilo Lwabona <bilo.lwabona@gmail.com>
2018-04-10 04:23:09 +0200

    fixed dev:demo

--- webpack.docs.config.js
+++ webpack.docs.config.js
@@ -1,53 +1,58 @@
-const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Visualizer = require('webpack-visualizer-plugin');
-const path = require('path');
-const DIST = path.resolve(__dirname, 'demo/');
-const SRC = path.resolve(__dirname, 'src/');
-
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-var config = {
-    entry: {
-        main: SRC + '/_demo/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, './docs/'),
-        filename: 'docs.js',
-        libraryTarget: 'umd'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }, {
-                test: /\.(css|scss)$/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader']
-            }, {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'file-loader?name=assets/[name].[ext]'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: './src/_demo/index.html', filename: 'index.html', inject: 'body' }),
-        new StaticSiteGeneratorPlugin({
-            paths: [
-              '/hello/',
-              '/world/'
-            ],
-            locals: {
-              greet: 'Hello'
-            }
-          })
-    ],
-    devServer: {
-        historyApiFallback: true,
-        stats: 'minimal'
-    }
-};
-
+const webpack = require('webpack'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const Visualizer = require('webpack-visualizer-plugin'); 
+const path = require('path'); 
+const DIST = path.resolve(__dirname, 'demo/'); 
+const SRC = path.resolve(__dirname, 'src/'); 
+const CopyWebpackPlugin = require('copy-webpack-plugin'); 
+ 
+var config = { 
+    devtool: 'source-maps', 
+    entry: { 
+        main: SRC + '/_demo/index.js' 
+    }, 
+    output: { 
+        path: DIST, 
+        publicPath: '/', 
+        filename: 'demo.js' 
+    }, 
+    // resolve: { 
+    //     modules: [ 
+    //         path.resolve('./'), 
+    //         path.resolve('./src/'), 
+    //         path.resolve('./node_modules') 
+    //     ] 
+    // }, 
+    module: { 
+        rules: [ 
+            { 
+                test: /\.(js|jsx)$/, 
+                loader: 'babel-loader', 
+                exclude: /node_modules/ 
+            }, { 
+                test: /\.(css|scss)$/, 
+                loaders: ['style-loader', 'css-loader', 'sass-loader'] 
+            }, { 
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/, 
+                loader: 'file-loader?name=assets/[name].[ext]' 
+            } 
+        ] 
+    }, 
+    plugins: [ 
+        new Visualizer({filename: './stats.html'}), 
+        new HtmlWebpackPlugin({template: './src/_demo/index.html', filename: 'index.html', inject: 'body'}), 
+        // new CopyWebpackPlugin([ 
+        //     { 
+        //         from: 'node_modules/highlight.js/styles/monokai.css', 
+        //         to: './scss/' 
+        //     } 
+        // ]) 
+    ], 
+    devServer: { 
+        historyApiFallback: true, 
+        stats: 'minimal' 
+    }, 
+    // node: {     fs: 'empty' } 
+}; 
+ 
 module.exports = config;
